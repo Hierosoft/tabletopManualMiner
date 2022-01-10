4ee11fcb508ff0e12ded5235df78d35a87655960
poikilos <7557867+poikilos@users.noreply.github.com>
2022-01-09 23:47:49 -0500

    Add partial monster parsing code.

--- srd/__init__.py
+++ srd/__init__.py
@@ -3,6 +3,8 @@
 import os
 import platform
 import sys
+import json
+import csv
 
 def prerr(msg):
     sys.stderr.write("{}\n".format(msg))
@@ -24,6 +26,8 @@ except ModuleNotFoundError:
 
 from io import StringIO
 
+alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 profile = None
 if platform.system() == "Windows":
     profile = os.environ['USERPROFILE']
@@ -31,7 +35,30 @@ else:
     profile = os.environ['HOME']
 
 
-def generateMeta(path, pageid=None):
+def setAllPageNumbers(device, pageid, pageNumberStr):
+    pageN = None
+    try:
+        pageN = int(pageNumberStr)
+    except ValueError as ex:
+        raise ValueError("WARNING: The page number for page id"
+                         " {} is unknown since the last text"
+                         " on the page is not an integer:"
+                         " \"{}\""
+                         "".format(pageid, pageNumberStr))
+        return
+
+    for chunk in device.chunks:
+        if chunk.pageid == pageid:
+            chunk.pageN = pageN
+
+
+def noParens(s):
+    if s.startswith("(") and s.endswith(")"):
+        s = s[1:-1]
+    return s
+
+
+def generateMeta(path, pageid=None, colStarts=None, max_pageid=None):
     """
     This function is based on code from
     lindblandro's Oct 4 '13 at 10:33 answer
@@ -50,27 +77,211 @@ def generateMeta(path, pageid=None):
     device = PDFPageDetailedAggregator(
         rsrcmgr,
         laparams=laparams,
-        colStarts=[57.6, 328.56],
+        colStarts=colStarts,
     )
     interpreter = PDFPageInterpreter(rsrcmgr, device)
 
+    UNKNOWN = 0
+    CONTEXT_MONSTERS = 1
+    CONTEXT_APPENDIX_A = 2
+    context = UNKNOWN
+
     for page in PDFPage.create_pages(doc):
         if (pageid is None) or (pageid==page.pageid):
-            print("page: {}".format(dir(page)))
+            # print("page: {}".format(dir(page)))
+            # ^ page: ['INHERITABLE_ATTRS', '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'annots', 'attrs', 'beads', 'contents', 'create_pages', 'cropbox', 'debug', 'doc', 'get_pages', 'lastmod', 'mediabox', 'pageid', 'resources', 'rotate']
+            progressTotalStr = ""
+            percentStr = ""
+            if max_pageid is not None:
+                progressTotalStr = "/" + str(max_pageid)
+                percent = float(page.pageid) / float(max_pageid) * 100
+                percentStr = " ({}%)".format(int(percent))
+                sys.stderr.write("\r")  # overwrite
+            sys.stderr.write("{}{}{}    "
+                             "".format(page.pageid, progressTotalStr,
+                                       percentStr))
+            sys.stderr.flush()
             interpreter.process_page(page)
             device.get_result()  # receive LTPage (runs receive_layout)
+            # NOTE: There is no point in iterating chunks here,
+            # because the list stored in device.chunks will grow on each
+            # iteration and the page numbers are not distinguished yet.
             if pageid is not None:
                 break
+    sys.stderr.write("\n")
+    sys.stderr.flush()
 
     # pprint(device.rows)
-    UNKNOWN = 0
-    MONSTERS = 1
-    context = MONSTERS
-    for frag in device.frags:
-        if "Monsters (A)" in frag.text:
-            context = MONSTERS
-            print("Found Monsters: {}".format(frag.text))
-        print(frag.text)
+    context = UNKNOWN
+    prevChunk = None
+    pageN = None
+
+    for chunk in device.chunks:
+        if prevChunk is not None:
+            if prevChunk.pageid != chunk.pageid:
+                setAllPageNumbers(device, prevChunk.pageid,
+                                  prevChunk.text)
+                # Assume that the last text on the page is the (visible)
+                # page number if it is a number.
+                # print("page {} ended with {}"
+                #       "".format(prevChunk.pageid, prevChunk.text))
+                # ^ usually the page number is 1 higher than the pageid.
+        prevChunk = chunk
+
+    if prevChunk is not None:
+        setAllPageNumbers(device, prevChunk.pageid, prevChunk.text)
+
+    monsterLetterHeadings = []
+    for letter in alphabetUpper:
+        monsterLetterHeadings.append("Monsters ({})".format(letter))
+    monster = None
+    monsters = []
+    for chunk in device.chunks:
+        if "Monsters (A)" in chunk.text:
+            # frag font: 'DXJJCX+GillSans-SemiBold' 21.474000000000046
+            if context != CONTEXT_MONSTERS:
+                context = CONTEXT_MONSTERS
+                print("Found Monsters: \"{}\" p. {} id {} font {} size {}"
+                      "".format(chunk.text, chunk.pageN, chunk.pageid, chunk.fontName, chunk.fontSize))
+                '''
+                for frag in chunk.fragments:
+                    print("- \"{}\"".format(frag.text))
+                    print("  font: '{}' {}"
+                          "".format(frag.fontname, frag.size))
+                '''
+        elif "Appendix PH-A:" in chunk.text:
+            # frag font: 'DXJJCX+GillSans-SemiBold' 30.922559999999976
+            if context != CONTEXT_APPENDIX_A:
+                context = CONTEXT_APPENDIX_A
+                print("Found end of Monsters: \"{}\" page {} id {} font {} size {}"
+                      "".format(chunk.text, chunk.pageN, chunk.pageid, chunk.fontName, chunk.fontSize))
+                print("Section: \"{}\"".format(chunk.text))
+                '''
+                for frag in chunk.fragments:
+                    print("- \"{}\"".format(frag.text))
+                    print("  font: '{}' {}"
+                          "".format(frag.fontname, frag.size))
+                '''
+        # Known SRD 5.1 styles:
+        #
+        isOneFrag = len(chunk.fragments) == 1
+        statName = None
+        oneFrag = None
+        if isOneFrag:
+            oneFrag = chunk.fragments[0]
+        statHeaders = ["STR", "CHA", "CON", "DEX", "WIS", "INT"]
+        NameHeader = 'name'
+        if context == CONTEXT_MONSTERS:
+            if statName is not None:
+                if monster is not None:
+                    # such as challenge
+                    # font: 'LUFRKP+Calibri' 13.116719999999987
+                    if statName == "INT":
+                        print("- found stats")
+                        parts = chunk.text.split()
+                        if len(parts) != 12:
+                            raise ValueError("Expected 12 parts"
+                                             " (6 like \"stat (mod)\")"
+                                             " after INT but got: "
+                                             "\"{}\""
+                                             "".format(statName))
+                        offset = 0
+                        for statH in statHeaders:
+                            monster[statH] = int(parts[0+offset])
+                            statTitle = statH.title() + "Mod"
+                            statMod = int(noParens(parts[1+offset]))
+                            monster[statTitle] = statMod
+                            offset += 2
+                    elif statName in statHeaders:
+                        # Do nothing for each header (Wait for next row)
+                        pass
+                    else:
+                        print("- found \"{}\"".format(statName))
+                        monster[statName] = chunk.text
+                else:
+                    print("WARNING: A stat was not under a monster in"
+                          " \"{}\"".format(chunk.text))
+                statName = None
+            elif chunk.oneStyle('WWROEK+Calibri-Bold', 16.1399):
+                # ^ Actually 16.139999999999986, but take advantage of
+                #   oneStyle's default rounding.
+                if monster is not None:
+                    monsters.append(monster)
+                monster = {
+                    NameHeader: chunk.text
+                }
+                print("{}:".format(monster[NameHeader].strip()))
+            elif chunk.oneStyle('DXJJCX+GillSans-SemiBold', 16.60656):
+                # Monster type subsection
+                if monster is not None:
+                    monsters.append(monster)
+                    monster = None
+                print("Monsters of type: \"{}\"".format(chunk.text))
+                for frag in chunk.fragments:
+                    print("- \"{}\"".format(frag.text))
+                    print("  font: '{}' {}"
+                          "".format(frag.fontname, frag.size))
+                monster = None
+            elif chunk.startStyle('WWROEK+Calibri-Bold', 13.2348):
+                # 13.234800000000064
+                if monster is None:
+                    print("Unknown stat: \"{}\"".format(chunk.text))
+                    print("  len(fragments): {}"
+                          "".format(len(chunk.fragments)))
+                    for frag in chunk.fragments:
+                        print("  - unknown fragment \"{}\""
+                              "".format(frag.text))
+                        print("    font: '{}' {}"
+                              "".format(frag.fontname, frag.size))
+                else:
+                    statName = chunk.fragments[0].text
+                    if len(chunk.fragments) == 2:
+                        monster[statName] = chunk.fragments[1].text
+                    else:
+                        # statName = chunk.text
+                        print("Unparsed stat: \"{}\""
+                              "".format(chunk.text))
+                        print("  len(fragments): {}"
+                              "".format(len(chunk.fragments)))
+                        for frag in chunk.fragments:
+                            print("  - unknown fragment \"{}\""
+                                  "".format(frag.text))
+                            print("    font: '{}' {}"
+                                  "".format(frag.fontname, frag.size))
+
+            elif monster is not None:
+                print("Unknown chunk: \"{}\"".format(chunk.text))
+                print("  len(fragments): {}"
+                      "".format(len(chunk.fragments)))
+                for frag in chunk.fragments:
+                    print("  - unknown fragment \"{}\""
+                          "".format(frag.text))
+                    print("    font: '{}' {}"
+                          "".format(frag.fontname, frag.size))
+        prevChunk = chunk
+
+    if monster is not None:
+        monsters.append(monster)
+    jsonName = 'monsters.json'
+    jsonPath = os.path.realpath(jsonName)
+    with open(jsonPath, 'w') as outs:
+        json.dump(monsters, outs, indent=2)  # sort_keys=True)
+        # If there are errors, ensure only simple types not classes are
+        # stored in the object.
+    print("* wrote \"{}\"".format(jsonPath))
+    tableHeaders = [NameHeader, "Challenge", "Languages", "Armor Class", "Hit Points", "Saving Throws", "Speed", "Skills", "Senses"]
+    csvName = 'monsters.csv'
+    csvPath = os.path.realpath(csvName)
+    with open(csvPath, 'w') as outs:
+        writer = csv.writer(outs)
+        writer.writerow(tableHeaders)
+        for monster in monsters:
+            row = []
+            for header in tableHeaders:
+                got = monster.get(header)
+                row.append(got)
+            writer.writerow(row)
+    print("* wrote \"{}\"".format(csvPath))
 
 
 def main():
@@ -84,7 +295,12 @@ def main():
         print("{} is missing. Download it and"
               " run this from that directory."
               "".format(srcPath))
-    generateMeta(srcPath, pageid=None)
+    generateMeta(
+        srcPath,
+        pageid=None,
+        colStarts=[57.6, 328.56],
+        max_pageid=1694,
+    )
 
 
 if __name__ == "__main__":
--- srd/pdfdetails.py
+++ srd/pdfdetails.py
@@ -14,6 +14,12 @@ except ModuleNotFoundError:
     prerr("  pdfminer")
     exit(1)
 
+try:
+    input = raw_input
+except NameError:
+    # Python 3
+    pass
+
 
 class BBox:
     def __init__(self, bbox):
@@ -27,14 +33,116 @@ class BBox:
 
 
 class DocFragment:
-    def __init__(self, pageid, column, bbox, text):
+    def __init__(self, text, fontname, size):
+        self.text = text
+        self.fontname = fontname
+        self.size = size
+
+    def sameStyle(self, fragment):
         """
-        bbox: (x1, y1, x2, y2)
+        Is same fontname and size.
+        """
+        ffn = fragment.fontname
+        ffs = fragment.size
+        return (ffs == self.size) and (ffn == self.fontname)
+
+    def clean(self):
+        # It may be odd like:
+        # "around \r  the \r  bend" (not actual example but spacing is)
+        # Therefore:
+        self.text = " ".join(self.text.split()).strip()
+        # ^ (strip with no param automatically uses any group of
+        #    whitespaces as a single delimiter)
+
+
+class DocChunk:
+    def __init__(self, pageid, column, bbox, text, fontName=None,
+                 fontSize=None, fragments=None, annotations=None):
+        """
+        Sequential arguments:
+        bbox -- The bounding box of the text in cartesian coordinates
+            (larger is further toward the top of the document) in the
+            format (x1, y1, x2, y2).
+
+        Keyword arguments:
+        fontName -- Only set if all fragments have same fontname.
+        fontSize -- Only set if all fragments have same font size.
+        fragments -- DocFragment objects representing parts of the chunk
+            (usually words) that differ in font size or font name.
+        annotations -- LTAnno objects (defined in pdfminer.layout)
         """
         self.pageid = pageid
         self.column = column
         self.bbox = BBox(bbox)
         self.text = text
+        self.fontSize = fontSize
+        self.fontName = fontName
+        self.fragments = fragments
+        self.annotations = annotations
+
+        self.pageN = None  # Set this later based on the visible number.
+
+    def groupFragments(self):
+        """
+        Combine fragments that are in a row and share the same fontname
+        and size.
+        """
+        fragments = []
+        thisFrag = None
+        for fragment in self.fragments:
+            if (thisFrag is None) or (not fragment.sameStyle(thisFrag)):
+                if thisFrag is not None:
+                    # Append the finished fragment.
+                    thisFrag.clean()
+                    fragments.append(thisFrag)
+                thisFrag = DocFragment(
+                    fragment.text,
+                    fragment.fontname,
+                    fragment.size,
+                )
+            else:
+                thisFrag.text += fragment.text
+
+        if thisFrag is not None:
+            # Append the last fragment.
+            thisFrag.clean()
+            fragments.append(thisFrag)
+        self.fragments = fragments
+
+    def oneStyle(self, fontname, size, decimalPlaces=2, index=None):
+        """
+        The DocChunk only has one fragment (or you specified index)
+        and it is in the specified style.
+
+        Keyword arguments:
+        decimalPlaces -- Round sizes to the given number of decimal
+            places before comparing them.
+        index -- Check only at this index (allows fragments to contain
+            more than one fragment).
+        """
+        if index is None:
+            if len(self.fragments) != 1:
+                return False
+            index = 0
+        frag = self.fragments[index]
+        fSize = round(frag.size, decimalPlaces)
+        size = round(size, decimalPlaces)
+        '''
+        if size != fSize:
+            print("size {} != {}".format(fSize, size))
+        if fontname != frag.fontname:
+            print("fontname {} is not {}"
+                  "".format(frag.fontname, fontname))
+        '''
+        return (frag.fontname == fontname) and (fSize == size)
+
+    def startStyle(self, fontname, size, decimalPlaces=2):
+        return self.oneStyle(
+            fontname,
+            size,
+            decimalPlaces=decimalPlaces,
+            index=0,
+        )
 
 
 class PDFPageDetailedAggregator(PDFPageAggregator):
@@ -50,7 +158,7 @@ class PDFPageDetailedAggregator(PDFPageAggregator):
     def __init__(self, rsrcmgr, pageno=1, laparams=None,
                  colStarts=None):
         PDFPageAggregator.__init__(self, rsrcmgr, pageno=pageno, laparams=laparams)
-        self.frags = []
+        self.chunks = []
         self.colStarts = colStarts
         if self.colStarts is not None:
             print("columns: {}".format(len(self.colStarts)))
@@ -63,11 +171,55 @@ class PDFPageDetailedAggregator(PDFPageAggregator):
                     render(child, page_number)
             elif isinstance(item, LTTextLine):
                 child_str = ''
+                fontSize = None
+                fontName = None
+                fontSizes = []
+                fontNames = []
+                warnings = []
+                parts = []
+                fragments = []
+                annotations = []
                 for child in item:
-                    if isinstance(child, (LTChar, LTAnno)):
+                    strp = None
+                    if isinstance(child, LTChar):
                         child_str += child.get_text()
+                        strp = child.get_text().strip()
+                        # and (len(strp) > 0)
+                        if fontName is not None:
+                            if fontName != child.fontname:
+                                warnings.append("mixed fontName")
+                        if fontSize is not None:
+                            if fontSize != child.size:
+                                warnings.append("mixed fontSize")
+                        fontName = child.fontname
+                        fontSize = child.size
+                        frag = DocFragment(
+                            child.get_text(),
+                            child.fontname,
+                            child.size,
+                        )
+                        fragments.append(frag)
+                        # fontNames.append(fontName)
+                        # fontSizes.append(fontSize)
+                        parts.append(strp)
+                    elif isinstance(child, LTAnno):
+                        child_str += child.get_text()
+                        strp = child.get_text().strip()
+                        annotations.append(child)
+
+
                 child_str = ' '.join(child_str.split()).strip()
                 if child_str:
+                    if len(warnings) > 0:
+                        """
+                        print("Warnings in \"{}\":"
+                              " {}: fonts {} sizes {} parts {}"
+                              "".format(child_str, warnings, fontNames,
+                                        fontSizes, parts))
+                        input("Press enter to continue...")
+                        """
+                        fontSize = None
+                        fontName = None
                     col = None
                     cols = 0
                     if self.colStarts is not None:
@@ -82,17 +234,35 @@ class PDFPageDetailedAggregator(PDFPageAggregator):
                     else:
                         raise ValueError("Only a list of length 1 (same as None) or 2"
                                          " is implemented for \"colStarts\".")
-                    frag = DocFragment(
+                    # if isinstance(child, LTChar):
+                    '''
+                    try:
+                        fontName = child.fontname
+                        fontSize = child.size
+                        # Avoid "AttributeError:
+                        # 'LTAnno' object has no attribute 'fontname'"
+                    except AttributeError as ex:
+                        print("dir(LTTextLine): {}".format(dir(LTTextLine)))
+                        print("dir(child): {}".format(dir(child)))
+                        raise ex
+                    '''
+
+                    chunk = DocChunk(
                         page_number,
                         col,
                         item.bbox,
                         child_str,
+                        fontName=fontName,
+                        fontSize=fontSize,
+                        fragments=fragments,
+                        annotations=annotations,
                     )
-                    self.frags.append(frag)
+                    chunk.groupFragments()
+                    self.chunks.append(chunk)
                 for child in item:
                     render(child, page_number)
             return
         render(ltpage, self.page_number)
         self.page_number += 1
-        self.frags = sorted(self.frags, key = lambda f: (f.pageid, f.column, -f.bbox.y1))
+        self.chunks = sorted(self.chunks, key = lambda f: (f.pageid, f.column, -f.bbox.y1))
         self.result = ltpage
